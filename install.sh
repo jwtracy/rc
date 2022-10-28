8b56eae29b8ebca7eb3d7b97815cb65c9fc70ce3
John Tracy <jwtracy@verily.com>
2022-10-28 13:03:20 -0700

    Install Helm

--- install.sh
+++ install.sh
@@ -83,6 +83,9 @@ kubectlinstall() {
     curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
     sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
     kubectl completion bash >> ~/.bashrc
+    curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+    chmod 700 get_helm.sh
+    ./get_helm.sh
     popd
 }
 
